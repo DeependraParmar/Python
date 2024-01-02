ff741159f3ce11033060406cfebcc056eeaba1a0
DeependraParmar <deependraparmar1@gmail.com>
2024-01-02 21:26:15 +0530

    Type Casting in Python

---
+++ 09_typeCasting.py
@@ -0,0 +1,9 @@
+# Typecasting is the technique of converting a datatype into other datatype. It provides various functions like int(), float(), str(), ord(), hex(), oct(), list(), dict(), set(), tuple(), etc. All of these functions will try to convert the variable of one type into  other.
+# 1. Implicit typecasting: automatically done by python.
+# 2. Explicit typecasting: done by the developer forcefully.
+
+a = "1"
+b = "2"
+
+print
+print(int(a) + int(b))
