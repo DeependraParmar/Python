e457f43219af3402f30e4b85d095e96372592a54
DeependraParmar <deependraparmar1@gmail.com>
2024-01-02 20:02:11 +0530

    Comments and more

---
+++ 05_commentsAndMore.py
@@ -0,0 +1,13 @@
+print("Hello, \n I am fine") # \n is an escape sequence character for new line
+print("Hello \"I am a good boy\" ")
+
+
+'''
+This is a multi-line comment
+for this file
+'''
+
+
+# print(object(s), sep=seperator, end=end, file=file, flush=flush)
+
+print("Hello", 6,6,7,8, sep='   ', end="0099") 
