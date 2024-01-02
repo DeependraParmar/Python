9eef7e88545c9fe40b48adfa2014f87a5caedf2e
DeependraParmar <deependraparmar1@gmail.com>
2024-01-02 20:19:20 +0530

    Variables and Datatypes in Python

---
+++ 06_variables&DataTypes.py
@@ -0,0 +1,29 @@
+"""In Python, Everything is an OBJECT """
+
+# variables are same as the container for holding the data, get some memory allocated in the memory and assigning it a value
+
+a = 16749392674632827467 # int or number datatype
+b = "Deependra Parmar" # string datatype
+c = True # boolean datatype
+d = None # none datatype
+e = 1.343
+f = complex(2,4)
+
+
+# Datatype of a particular variable can be determined by a function type()
+
+print(a,b,c,d,e,f)
+print("The type of a is: ", type(a))
+print("The type of b is: ", type(b))
+print("The type of c is: ", type(c))
+print("The type of d is: ", type(d))
+print("The type of e is: ", type(e))
+print("The type of f is: ", type(f))
+
+
+# We also have some of the sequenced datatypes like lists and tuples
+# 1. lists are ordered, mutable collection of data with elements seperated with comma enclosed in []
+# 1. lists are ordered, immutable collection of data with elements seperated with comma enclosed in ()
+
+
+# We also have a mapped datatype Dictionary that is the collection of key value pairs in {}
