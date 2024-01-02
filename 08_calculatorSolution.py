10f4c3d8f9ebd9bc71704aceb93669349ea00277
DeependraParmar <deependraparmar1@gmail.com>
2024-01-02 20:27:43 +0530

    Calculator Solution

---
+++ 08_calculatorSolution.py
@@ -0,0 +1,11 @@
+a = int(input("Enter the value of a: "))
+b = int(input("Enter the value of b: "))
+print()
+
+print("Addition of a and b is: ", (a+b))
+print("Subtraction of a and b is: ", (a-b))
+print("Multiplication of a and b is: ", (a*b))
+print("Division of a and b is: ", (a/b))
+print("Modulo of a and b is: ", (a%b))
+print("Floor Division of a and b is: ", (a//b))
+print("Exponentiation of a and b is: ", (a**b))
