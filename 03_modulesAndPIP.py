25634370d8ced21e73510e0be98559b61b8098c4
DeependraParmar <deependraparmar1@gmail.com>
2024-01-02 17:55:16 +0530

    Pip and Modules in Python

---
+++ 03_modulesAndPIP.py
@@ -0,0 +1,4 @@
+# Modules are the reusable packages that you can use while writing the code in python. Python has built-in modules which you need not to install, they are already present in the python. 
+# External modules need to bring locally before working with them
+
+# PIP: Preferred Installer Program is the package manager for python which helps you install the python packages and run the python scripts.
