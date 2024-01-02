ad531c7d139b00ebc022d791f318fcc9e67687f6
DeependraParmar <deependraparmar1@gmail.com>
2024-01-02 21:36:55 +0530

    Taking User Input in Python

---
+++ 10_input.py
@@ -0,0 +1,11 @@
+# a = input("Enter your name here: ")
+# print("Your name is: ", a)
+
+# ack = input("Is it right [Yes]/[No]: ")
+
+
+x = int(input("Enter the first number: "))
+y = int(input("Enter the second number: "))
+
+print()
+print("Sum of x and y is: ", (x+y))
