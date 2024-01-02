0dce7963e68b889739d3338ccc90be2ec3b18ecf
DeependraParmar <deependraparmar1@gmail.com>
2024-01-02 17:31:17 +0530

    Introduction to Python

---
+++ 01_introToPython.py
@@ -0,0 +1,4 @@
+# This is the first python file that we are going to create in the journey of learning python
+
+print("Hello, Jai Siya Ram!")
+print("I am ", 5)
--- python.py
+++
@@ -1,3 +0,0 @@
-print("Hello World")
-arr = [1,2,3,4,5,6,7,8]
-print("Array is: " , arr)
