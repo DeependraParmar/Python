71dadb3c549d2567ab65084dfccf74429c5fe3bb
DeependraParmar <deependraparmar1@gmail.com>
2024-01-02 20:23:30 +0530

    Calculator: Exercise

---
+++ 07_calculator.py
@@ -0,0 +1,8 @@
+# Take two numbers as input and perform 
+# 1. Addition(+)
+# 2. Subtraction(-)
+# 3. Multiplication(*)
+# 4. Division(/)
+# 5. Modulo(%)
+# 6. Floor Division(//)
+# 7. Exponentiation(**)
