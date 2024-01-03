b7062ce8c2e8991ffc02e1945eab596e41a7627f
DeependraParmar <deependraparmar1@gmail.com>
2024-01-03 19:31:54 +0530

    If Else Statements

---
+++ 14_ifElse.py
@@ -0,0 +1,11 @@
+a = int(input("Enter your age: "))
+print()
+
+#Conditional Operators: > ,< ,>= ,<= ,!= ,==
+
+if(a>18):
+    print("You can drive 🚗")
+elif(a==18):
+    print("Get your licence and then you can drive")
+else:
+    print("You cannot drive🚫")
