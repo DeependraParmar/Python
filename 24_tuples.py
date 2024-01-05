a35f995bd72b4263ffa9b4f5cba1c3087173c487
DeependraParmar <deependraparmar1@gmail.com>
2024-01-05 12:38:37 +0530

    Tuples in Python

---
+++ 24_tuples.py
@@ -0,0 +1,9 @@
+# Tuples are as same as the lists with just one difference that it is immutable in nature.
+name = (1,2,3,4,5)
+# name[0] = 10 # gives error giving object does not support item assignment.
+print("Tuple name is: ", name)
+
+
+# indexing, slicing works same as that of the list
+# you can also check for an item using if statement.
+# 
