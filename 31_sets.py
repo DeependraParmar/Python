63ef668043e2b00857cdfde0b562eca629056b4a
DeependraParmar <deependraparmar1@gmail.com>
2024-01-06 12:48:04 +0530

    Sets in Python

---
+++ 31_sets.py
@@ -0,0 +1,9 @@
+# Sets are the unordered datatype that store objects which is used to store different type of items where all the items are unique. Items are seperated by , and are enclosed in {}
+# They are immutable in nature 
+# Since, they are unordered,indexing is of no use
+
+s1 = {True, "Ram", 39.20943, 'a'}
+print(s1)
+
+for i in s1:
+    print(i)
